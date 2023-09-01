d2c2c6a958646bcaf9ab358e394188fc57691bbb
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-09-01 17:20:28 +0530

    sidebar comp created

---
+++ src/pages/landingPage/LandingPage.tsx
@@ -0,0 +1,11 @@
+import SideBar from '../../components/sideBar/SideBar';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <SideBar />
+    </div>
+  );
+};
+
+export default LandingPage;
