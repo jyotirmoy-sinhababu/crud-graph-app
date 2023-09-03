bc66c8efd87b08c6fba986892977b7b09a24b4b9
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-09-03 18:34:16 +0530

    tailwind in stalled

--- src/components/lineChart/LineChart.tsx
+++ src/components/lineChart/LineChart.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from 'react-query';
+import { Line } from 'react-chartjs-2';
 
 const LineChart = () => {
   const getCovidData = async () => {
@@ -10,9 +11,50 @@ const LineChart = () => {
 
   const { data, error, isLoading } = useQuery('covidData', getCovidData);
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching data</div>;
+  }
+
   console.log(data);
 
-  return <div>LineChart</div>;
+  const covidData = data.cases;
+
+  const labels = Object.keys(covidData);
+  const cases = Object.values(covidData);
+
+  const chartConfigs = {
+    scales: {
+      x: {
+        type: 'time', // Use 'time' scale for dates
+        time: {
+          unit: 'day', // Adjust the time unit as needed
+        },
+      },
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  const chartData = {
+    labels,
+    datasets: [
+      {
+        fill: false,
+        label: 'Total Cases',
+        data: cases,
+        borderColor: 'rgba(255, 255, 255, 0.6)',
+      },
+    ],
+  };
+
+  return (
+    <div>{Object.keys(covidData)?.length > 0 && <Line data={chartData} />}</div>
+  );
 };
 
 export default LineChart;
